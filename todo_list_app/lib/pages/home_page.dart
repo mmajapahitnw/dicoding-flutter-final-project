d521bceb17d36a8250a4e47c671936e8a2eed146
Majapahit Wisisono <122672518+mmajapahitnw@users.noreply.github.com>
2023-10-17 19:42:50 +0700

    Update home_page.dart

--- todo_list_app/lib/pages/home_page.dart
+++ todo_list_app/lib/pages/home_page.dart
@@ -10,6 +10,14 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            'TO DO List',
+          ),
+        ),
+      )
+    );
   }
 }
