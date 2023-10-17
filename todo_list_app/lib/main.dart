2c06c9c7c3475f04106fda25c39e0319728ac23f
Majapahit Wisisono <122672518+mmajapahitnw@users.noreply.github.com>
2023-10-17 19:11:54 +0700

    Create blank scaffold

--- todo_list_app/lib/main.dart
+++ todo_list_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list_app/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow.shade300),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(),
     );
   }
 }
