6599c92a20aaf1dbe873dcfb5318f6f9a8387125
Majapahit Wisisono <122672518+mmajapahitnw@users.noreply.github.com>
2023-10-18 11:58:25 +0700

    Update todo_cards.dart

---
+++ todo_list_app/lib/components/todo_cards.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TodoCard extends StatelessWidget {
+  const TodoCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
