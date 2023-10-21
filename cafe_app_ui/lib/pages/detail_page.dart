140995eb3593b763ca0da922261dc26e91ece597
Majapahit Wisisono <122672518+mmajapahitnw@users.noreply.github.com>
2023-10-21 16:22:40 +0700

    Update detail_page.dart

--- cafe_app_ui/lib/pages/detail_page.dart
+++ cafe_app_ui/lib/pages/detail_page.dart
@@ -6,7 +6,41 @@ class DetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //appBar: ,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Image.asset('lib/images/allen.jpg'),
+                SafeArea(
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        CircleAvatar(
+                          backgroundColor: Colors.grey[800],
+                          child: Icon(
+                            Icons.chevron_left,
+                            color: Colors.white,
+                          ),
+                        ),
+                        CircleAvatar(
+                          backgroundColor: Colors.grey[800],
+                          child: Icon(
+                            Icons.favorite,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
