272b42e6d2c01c245d0c6ffb48545c3557f043fe
Majapahit Wisisono <122672518+mmajapahitnw@users.noreply.github.com>
2023-10-21 17:48:31 +0700

    Update detail_page.dart

--- cafe_app_ui/lib/pages/detail_page.dart
+++ cafe_app_ui/lib/pages/detail_page.dart
@@ -8,7 +8,6 @@ class DetailPage extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             // stack item image with back and favorite button
             Stack(
@@ -45,38 +44,151 @@ class DetailPage extends StatelessWidget {
 
             // container for item name
             Container(
-              height: 100,
+              height: 150,
               decoration: BoxDecoration(
-                color: Colors.grey,
+                color: Colors.grey[900],
                 borderRadius: BorderRadius.only(
                     bottomLeft: Radius.circular(20),
                     bottomRight: Radius.circular(20)),
+              ),
+              child: Row(
+                children: [
+                  Column(
+                    children: [
+                      Text('item_name'),
+                      Text('item_desc')
+                    ],
+                  )
+                ],
               ),
             ),
             SizedBox(height: 20),
 
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 24),
-              child: Text(
-                'Description',
-                style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 16,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Description',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 16,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Enjoy AWOR\'s spin on coffee_name. Made with love, a signature treat for weary soul.',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  SizedBox(height: 24),
+                  Text(
+                    'Size',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 16,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.grey[800],
+                          borderRadius: BorderRadius.circular(6),
+                        ),
+                        child: Padding(
+                          padding:
+                              EdgeInsets.symmetric(vertical: 8, horizontal: 40),
+                          child: Text('S'),
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.grey[800],
+                          borderRadius: BorderRadius.circular(6),
+                        ),
+                        child: Padding(
+                          padding:
+                              EdgeInsets.symmetric(vertical: 8, horizontal: 40),
+                          child: Text('M'),
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.transparent,
+                            borderRadius: BorderRadius.circular(6),
+                            border: Border.all(
+                              color: Colors.orange,
+                            )),
+                        child: Padding(
+                          padding:
+                              EdgeInsets.symmetric(vertical: 8, horizontal: 40),
+                          child: Text(
+                            'L',
+                            style: TextStyle(color: Colors.orange),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        height: 80,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 24, right: 24, bottom: 12),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 24),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Price'),
+                    RichText(
+                      text: TextSpan(
+                        text: 'IDR ',
+                        style: TextStyle(
+                          color: Colors.orange,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: 'harga_item',
+                            style: TextStyle(color: Colors.white)
+                          )
+                        ]
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ),
-            SizedBox(height: 10),
-
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              child: Text(
-                'Enjoy AWOR\'s spin on coffee_name. Made with love, a signature treat for weary soul.',
-                style: TextStyle(fontSize: 18),
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.orange,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Buy Now',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
               ),
-            ),
-
-            
-          ],
+            ],
+          ),
         ),
       ),
     );
