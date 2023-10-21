9c1208b7707bdcacdd0b8964ace9d5ea9b8937b9
Majapahit Wisisono <122672518+mmajapahitnw@users.noreply.github.com>
2023-10-21 11:03:25 +0700

    Update main.dart

--- cafe_app_ui/lib/main.dart
+++ cafe_app_ui/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
-      theme: ThemeData(brightness: Brightness.dark),
+      theme: ThemeData(brightness: Brightness.dark, primarySwatch: Colors.orange),
     );
   }
 }
