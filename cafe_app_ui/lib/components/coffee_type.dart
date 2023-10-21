926bd1a6216409f25d9ff0b5d1bd3bde42058a0f
Majapahit Wisisono <122672518+mmajapahitnw@users.noreply.github.com>
2023-10-21 12:12:48 +0700

    Update coffee_type.dart

--- cafe_app_ui/lib/components/coffee_type.dart
+++ cafe_app_ui/lib/components/coffee_type.dart
@@ -3,10 +3,23 @@ import 'package:flutter/material.dart';
 class CoffeeType extends StatelessWidget {
   final String name;
 
-  const CoffeeType({super.key, required this.name,});
+  const CoffeeType({
+    super.key,
+    required this.name,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Padding(padding: EdgeInsets.only(left: 24), child: Text(name,),);
+    return Padding(
+      padding: EdgeInsets.only(left: 24),
+      child: Text(
+        name,
+        style: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+          color: Colors.orange,
+        ),
+      ),
+    );
   }
 }
